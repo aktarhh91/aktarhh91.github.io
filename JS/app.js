c4ad4797bc012125dd7529285b9327c9908302c6
Student_GA102 <Student_GA102@Student-GA102.local>
2020-02-03 14:13:59 -0500

    first code

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+class ussShazam{
+    constructor(){
+
+        this.hull=
+        this.firepower=
+        class ussSwarch {
+            constructor(){
+             const hull= 20;
+              const firepower= 5;
+              const accuracy= .7;
+              }
+            1:23
+            let exTerra1 = new exTerra();
+            let exTerra2 = new exTerra();
+            let exTerra3 = new exTerra();
+            let exTerra4 = new exTerra();
+            let exTerra5 = new exTerra();
+            let exTerra6 = new exTerra();
+
